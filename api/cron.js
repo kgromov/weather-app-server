5538bc626d39efaf30c60f08b0b629f6a550d2b3
Konstantin <konstantin@revimedia.com>
2023-05-13 01:22:36 +0300

    Dummy commimt

--- api/cron.js
+++ api/cron.js
@@ -4,7 +4,7 @@ const temperatureService = require("../app/service/temperature-service");
 
 export default function handler(req, res) {
     mongoose.connect(dbConfig.uri, {useNewUrlParser: true, useUnifiedTopology: true})
-        .then(() => console.log('Successfully connected to MongoDB at ...'))
+        .then(() => console.log('[CRON] Successfully connected to MongoDB at ...'))
         .then(() => temperatureService.syncForToday())
         .then(res => res.status(200).json('Sync is finished'))
         .catch(e => console.error('Failed connected to MongoDB...', e));
