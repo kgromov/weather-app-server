4722f926608e3a8e4ba8d1cb69dc39009010fa11
konstantin <konst.gromov@gmail.com>
2024-03-25 02:18:50 +0200

    Skip calculate average if no measurements

--- app/model/dto.js
+++ app/model/dto.js
@@ -10,10 +10,12 @@ exports.TemperatureMeasurementsDto = class {
 
     constructor(date, dailyMeasurements) {
         this.date = date;
-        this.nightTemperature = this.constructor.calculatePartOfTheDayTemperature(0, 6, dailyMeasurements);
-        this.morningTemperature = this.constructor.calculatePartOfTheDayTemperature(6, 12, dailyMeasurements);
-        this.afternoonTemperature = this.constructor.calculatePartOfTheDayTemperature(12, 18, dailyMeasurements);
-        this.eveningTemperature = this.constructor.calculatePartOfTheDayTemperature(18, 24, dailyMeasurements);
+        if (dailyMeasurements?.length > 0) {
+            this.nightTemperature = this.constructor.calculatePartOfTheDayTemperature(0, 6, dailyMeasurements);
+            this.morningTemperature = this.constructor.calculatePartOfTheDayTemperature(6, 12, dailyMeasurements);
+            this.afternoonTemperature = this.constructor.calculatePartOfTheDayTemperature(12, 18, dailyMeasurements);
+            this.eveningTemperature = this.constructor.calculatePartOfTheDayTemperature(18, 24, dailyMeasurements);
+        }
     }
 
     static calculatePartOfTheDayTemperature(from, to, dailyMeasurements) {
