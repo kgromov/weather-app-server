d91a677fbc9743308c39a5eeff6bb28e486d0899
konstantin <konst.gromov@gmail.com>
2025-01-05 03:00:18 +0200

    Change server routes to /api

--- app/routes/weather-routes.js
+++ app/routes/weather-routes.js
@@ -2,38 +2,38 @@ const express = require("express");
 const weatherService = require('./../service/weather-service');
 const router = express.Router();
 
-router.get("/years", async (req, res) => {
+router.get("/api/years", async (req, res) => {
     const yearsToShow = await weatherService.getYearsToShow();
     res.status(200).json(yearsToShow);
 });
 
-router.get("/seasons", async (req, res) => {
+router.get("/api/seasons", async (req, res) => {
     const seasonsWeather = await weatherService.getYearsBySeasonsTemperature()
     res.status(200).json(seasonsWeather);
 });
 
-app.get("/weather/month", async (req, res) => {
+app.get("/api/weather/month", async (req, res) => {
     const seasonsWeather = await weatherService.getYearsByMonthTemperature()
     res.status(200).json(seasonsWeather);
 });
 
-router.get("/summary", async (req, res) => {
+router.get("/api/summary", async (req, res) => {
     const yearsWeather = await weatherService.getYearsSummary()
     res.status(200).json(yearsWeather);
 });
 
-router.get("/current", async (req, res) => {
+router.get("/api/current", async (req, res) => {
     const weatherForToday = await weatherService.getWeatherForToday();
     res.status(200).json(weatherForToday);
 });
 
-router.get("/single/:day", async (req, res) => {
+router.get("/api/single/:day", async (req, res) => {
     console.log(`Getting weather for day ${req.params.day}`);
     const weatherForToday = await weatherService.getWeatherAtDay(req.params.day)
     res.status(200).json(weatherForToday);
 });
 
-router.get("/:day", async (req, res) => {
+router.get("/api/:day", async (req, res) => {
     const weatherForToday = await weatherService.getWeatherDayInRange(req.params.day, req.query.years)
     res.status(200).json(weatherForToday);
 });
--- index.js
+++ index.js
@@ -21,59 +21,59 @@ app.use(cors(corsOptions));
 
 // establish connection
 mongoose.set('strictQuery', true);
-mongoose.connect(dbConfig.uri, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(dbConfig.uri)
     .then(() => console.log('Successfully connected to MongoDB ...'))
     .catch(e => console.error('Failed connected to MongoDB ...', e));
 
-app.get("/sync", async (req, res) => {
+app.get("/api/sync", async (req, res) => {
     const syncStatus = await temperatureService.syncForToday();
     res.status(syncStatus.code).json(syncStatus);
 });
 
-app.get("/isSynced", async (req, res) => {
+app.get("/api/isSynced", async (req, res) => {
     const isSynced = await temperatureService.isUpToDate();
     res.status(200).json(isSynced);
 });
 
-app.get("/weather/years", async (req, res) => {
+app.get("/api/weather/years", async (req, res) => {
     const yearsToShow = await weatherService.getYearsToShow();
     res.status(200).json(yearsToShow);
 });
 
-app.get("/weather/seasons", async (req, res) => {
+app.get("/api/weather/seasons", async (req, res) => {
     const seasonsWeather = await weatherService.getYearsBySeasonsTemperature()
     res.status(200).json(seasonsWeather);
 });
 
-app.get("/weather/months", async (req, res) => {
+app.get("/api/weather/months", async (req, res) => {
     const seasonsWeather = await weatherService.getYearsByMonthsTemperature()
     res.status(200).json(seasonsWeather);
 });
 
-app.get("/weather/summary", async (req, res) => {
+app.get("/api/weather/summary", async (req, res) => {
     const yearsWeather = await weatherService.getYearsSummary()
     res.status(200).json(yearsWeather);
 });
 
-app.get("/weather/current", async (req, res) => {
+app.get("/api/weather/current", async (req, res) => {
     const weatherForToday = await weatherService.getWeatherForToday();
     res.status(200).json(weatherForToday);
 });
 
-app.get("/weather/single/:day", async (req, res) => {
+app.get("/api/weather/single/:day", async (req, res) => {
     console.log(`Getting weather for day ${req.params.day}`);
     const weatherForToday = await weatherService.getWeatherAtDay(req.params.day)
     res.status(200).json(weatherForToday);
 });
 
-app.get("/weather/:day", async (req, res) => {
+app.get("/api/weather/:day", async (req, res) => {
     const weatherForToday = await weatherService.getWeatherDayInRange(req.params.day, req.query.years)
     res.status(200).json(weatherForToday);
 });
 
 const port = process.env.PORT | 9000;
 app.listen(port, () => {
-    console.log(`Server is started on post ${port}`);
+    console.log(`Server is started on port ${port}`);
 });
 
 // cron.schedule("*/5 * * * *", async () => {
