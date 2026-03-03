64b4be9453bd25e3eaecf168aebe64c5d1450db0
konstantin <konst.gromov@gmail.com>
2026-03-04 01:56:16 +0200

    One more attempt to apply vercel cron scheduler

--- app/api/route.js
+++ app/api/route.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const dbConfig = require("../app/config/db-config");
-const temperatureService = require("../app/service/temperature-service");
+const dbConfig = require("../config/db-config");
+const temperatureService = require("../service/temperature-service");
 
-export default function handler(req, res) {
-    mongoose.connect(dbConfig.uri, {useNewUrlParser: true, useUnifiedTopology: true})
+export async function handler(req, res) {
+    return mongoose.connect(dbConfig.uri)
         .then(() => console.log('[CRON] Successfully connected to MongoDB at ...'))
         .then(() => temperatureService.syncForToday())
         .then(async res => res.status(200).json('Sync is finished'))
